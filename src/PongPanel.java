9ff0cb805d4bb7eda7ca9dc19ebe537d18e08661
MinBlaster <jmanuelch1987@gmail.com>
2025-06-28 14:57:41 +0200

    Tercer commit - Estructura básica del juego - pala, pelota en movimiento y colisiones con la pala y paredes.

--- src/PongPanel.java
+++ src/PongPanel.java
@@ -1,20 +1,35 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
-public class PongPanel extends JPanel implements Runnable {
+public class PongPanel extends JPanel implements Runnable, KeyListener {
 
     //Declaramos las variables para la posición de la pelota.
     int ballX = 100;
     int ballY = 100;
     int ballSize = 20;
 
-    //Decalramos las variables para controlar la dirección del movimiento.
+    //Declaramos las variables para controlar la dirección del movimiento.
     int ballSpeedX = 2;
     int ballSpeedY = 2;
 
     //Declaramos el ancho y el alto de la ventana con final, para que no cambien nunca.
     final int PANEL_WIDTH = 800;
     final int PANEL_HEIGHT = 600;
+
+    //Declaramos las variables para la pala.
+    int playerX = 50; //Posición horizontal de la pala.
+    int playerY = 250; //Posición vertical.
+    int playerWidth = 20; //Ancho de la pala.
+    int playerHeight = 100; //Alto de la pala.
+
+
+    //Constructor.
+    public PongPanel(){
+        this.setFocusable(true); //Permite que el panel reciba eventos de teclado.
+        this.addKeyListener(this); //Le dice que escuche teclas en este panel.
+    }
 
     //Creo el método paintComponent() y lo sobrescribo.
     @Override
@@ -23,6 +38,9 @@ public class PongPanel extends JPanel implements Runnable {
 
         //Dibujamos la pelota con fillOval.
         g.fillOval(ballX, ballY, ballSize, ballSize);
+
+        //Dibujamos la pala con fillRect.
+        g.fillRect(playerX, playerY, playerWidth, playerHeight);
 
     }
 
@@ -38,6 +56,11 @@ public class PongPanel extends JPanel implements Runnable {
             ballX += ballSpeedX;
             ballY += ballSpeedY;
 
+            //Creamos la clase Rectangle con la pelota y la pala para determinar si se están tocando, para ello
+            //usamos .intersects. Lo meto dentro del bucle siempre uso la posición actualizada de la pelota.
+            Rectangle ball = new Rectangle(ballX, ballY, ballSize, ballSize);
+            Rectangle paddle = new Rectangle(playerX, playerY, playerWidth, playerHeight);
+
             //Condiciones, para que la bola choque con los bordes de la pantalla.
             if (ballX <= 0 || ballX >= PANEL_WIDTH - ballSize){
                 ballSpeedX *= -1;
@@ -45,6 +68,16 @@ public class PongPanel extends JPanel implements Runnable {
 
             if (ballY <= 0 || ballY >= PANEL_HEIGHT - ballSize){
                 ballSpeedY *= -1;
+            }
+
+            //Limitar la pala dentro del panel.
+            if (playerY < 0)playerY = 0;
+            if (playerY>PANEL_HEIGHT - playerHeight)playerY = PANEL_HEIGHT - playerHeight;
+
+
+            //Colisión de la pelota con la pala cuando se toquen.
+            if (ball.intersects(paddle)){
+                ballSpeedX *= -1;
             }
 
             //Volver a dibujar el panel.
@@ -57,6 +90,31 @@ public class PongPanel extends JPanel implements Runnable {
                 e.printStackTrace();
 
             }
+        }
+
+    }
+
+    //Implementamos los métodos obligatorios de KeyListener.
+    @Override
+    public void keyTyped (KeyEvent e){
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e){
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e){
+
+        if (e.getKeyCode()== KeyEvent.VK_W){
+            playerY -= 10;
+
+        }
+
+        if (e.getKeyCode()== KeyEvent.VK_S){
+            playerY += 10;
         }
 
     }
